ef897468eb2e7f2e05806a1c7bf724abf5744386
Pastor <sotoblanco263542@gmail.com>
2022-12-03 21:31:04 -0400

    week 11

--- 10-Severless/test.py
+++ 10-Severless/test.py
@@ -1,9 +1,11 @@
 import requests
 
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+#url = "https://w9oqgv678c.execute-api.us-west-2.amazonaws.com/test/predict"
+url = 'https://tpxtwbbm2oqkfpyqpj64orwspa0iwwnc.lambda-url.us-west-2.on.aws/predict'
 
-data = {'url': "https://upload.wikimedia.org/wikipedia/en/e/e9/GodzillaEncounterModel.jpg"}
+data = {"url": "https://upload.wikimedia.org/wikipedia/en/e/e9/GodzillaEncounterModel.jpg"}
 
 result = requests.post(url, json=data).json()
 
